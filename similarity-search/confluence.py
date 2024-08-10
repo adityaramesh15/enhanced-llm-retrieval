667832f0e30fcf9b065be3db55234fd242d765ca
Aditya Ramesh <adityaramesh15@gmail.com>
2024-08-09 19:29:14 -0500

    Loaded Space and Page IDs from Confluence

--- similarity-search/confluence.py
+++ similarity-search/confluence.py
@@ -2,11 +2,50 @@ import requests
 from requests.auth import HTTPBasicAuth
 import json
 import os
-
-url = "https://adityaramesh15.atlassian.net/wiki/api/v2/spaces/enhancedllmretrieval/pages"
-api_key = os.environ.get("CONFLUENCE_API_KEY")
-
-auth = HTTPBasicAuth("adityaramesh15@gmail.com", api_key)
+from dotenv import load_dotenv
 
 
+load_dotenv()
+api_url = os.getenv("CONFLUENCE_URL")
+api_key = os.getenv("CONFLUENCE_API_KEY")
+username = os.getenv("CONFLUENCE_USERNAME")
+auth = HTTPBasicAuth(username, api_key)
+
+
+def get_space_id():
+    url = f"{api_url}/spaces"
+    space_ids = {}
+    while url:
+        response = requests.get(url, auth=auth)
+        data = response.json()  
+       
+        for space in data['results']:
+            name = space['name']
+            space_ids[name] = space['id']
+        
+        url = data['_links'].get('next')  
+    return space_ids
+
+
+def get_page_ids(space_id):
+    url = f"{api_url}/spaces/{space_id}/pages"
+    page_ids = {}
+    while url:
+        response = requests.get(url, auth=auth)
+        data = response.json()  
+
+        for page in data['results']:
+            title = page['title']
+            page_ids[title] = page['id']
+
+        url = data['_links'].get('next')  
+    return page_ids
+
+
+def get_content(page_id):
+    ...
+
+
+def upsert_pages():
+    ...
 
