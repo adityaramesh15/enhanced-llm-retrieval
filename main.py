1e6f2ff0de916347bd40703ac4a4025b5ccb044d
Aditya Ramesh <adityaramesh15@gmail.com>
2024-08-19 19:56:44 -0500

    Updates and Fine tuning.

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from hybrid_search.database import Database
 from rag_llm.response import Response
 from multiprocessing import Process
 import os
+import sys
+import contextlib
 
 db = UpdateDatabase()
 semantic = SemanticSearch()
@@ -14,21 +16,21 @@ def run_update():
 
 if __name__ == "__main__": 
     session_id = 'test_session'
-    
-    # db.load_all()
+    db.load_all()
     os.environ['TOKENIZERS_PARALLELISM'] = 'true' 
 
     procs = []
     proc = Process(target=run_update)  
     procs.append(proc)
     proc.start()
+
     try:
         while True:
             query = input("Enter your query: ")
             matches = semantic.search(query)
             print("\nResponse:")
             print(response.query_model(session_id, query, matches))
-    
+
     except KeyboardInterrupt:
         print("\nExiting...")
     
@@ -38,5 +40,4 @@ if __name__ == "__main__":
             proc.terminate() 
             proc.join()
         
-
     print("Processes terminated. Exiting program.")
